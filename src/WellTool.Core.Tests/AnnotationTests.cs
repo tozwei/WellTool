63844854436fcabb54a55f635f81072b8f23144b
tozwei <tozwei@126.com>
2026-03-28 12:38:45 +0800

    编译通过

--- src/WellTool.Core.Tests/AnnotationTests.cs
+++ src/WellTool.Core.Tests/AnnotationTests.cs
@@ -187,55 +187,109 @@ namespace WellTool.Core.Tests
     // 注解扫描器测试
     public class AnnotationScannerTests
     {
-        // [Fact]
-        // public void TestElementAnnotationScanner()
-        // {
-        //     var scanner = new ElementAnnotationScanner();
-        //     var annotations = scanner.Scan(typeof(ClassWithAnnotation));
-        //     XAssert.NotNull(annotations);
-        // }
+        [AnnotationForTest(Value = "测试", Names = new[] { "测试1", "测试2" })]
+        [RepeatAnnotationForTest]
+        private class ClassWithAnnotation
+        {
+            public void TestMethod() {}
+        }
 
-        // [Fact]
-        // public void TestFieldAnnotationScanner()
-        // {
-        //     var scanner = new FieldAnnotationScanner();
-        //     var annotations = scanner.Scan(typeof(ClassWithFieldAnnotation));
-        //     XAssert.NotNull(annotations);
-        // }
+        [AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+        private class RootMetaAnnotation3 : Attribute
+        {}
 
-        // [Fact]
-        // public void TestGenericAnnotationScanner()
-        // {
-        //     var scanner = new GenericAnnotationScanner();
-        //     var annotations = scanner.Scan(typeof(ClassWithAnnotation));
-        //     XAssert.NotNull(annotations);
-        // }
+        [RootMetaAnnotation3]
+        [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
+        private class RootMetaAnnotation2 : Attribute
+        {}
 
-        // [Fact]
-        // public void TestMetaAnnotationScanner()
-        // {
-        //     var scanner = new MetaAnnotationScanner();
-        //     var annotations = scanner.Scan(typeof(RootAnnotation));
-        //     XAssert.NotNull(annotations);
-        // }
+        [RootMetaAnnotation2]
+        [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
+        private class RootMetaAnnotation1 : Attribute
+        {}
 
-        // [Fact]
-        // public void TestMethodAnnotationScanner()
-        // {
-        //     var scanner = new MethodAnnotationScanner();
-        //     var method = typeof(TargetClass).GetMethod("TestMethod");
-        //     XAssert.NotNull(method);
-        //     var annotations = scanner.Scan(method);
-        //     XAssert.NotNull(annotations);
-        // }
+        [RootMetaAnnotation3]
+        [RootMetaAnnotation1]
+        [AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+        private class RootAnnotation : Attribute
+        {}
 
-        // [Fact]
-        // public void TestTypeAnnotationScanner()
-        // {
-        //     var scanner = new TypeAnnotationScanner();
-        //     var annotations = scanner.Scan(typeof(ClassWithAnnotation));
-        //     XAssert.NotNull(annotations);
-        // }
+        [AnnotationForTest("TargetClass")]
+        private class TargetClass : TargetSuperClass, TargetSuperInterface
+        {
+            [AnnotationForTest("TargetClass")]
+            public override object TestMethod() { return new List<object>(); }
+        }
+
+        [AnnotationForTest("TargetSuperClass")]
+        private class TargetSuperClass : SuperInterface
+        {
+            [AnnotationForTest("TargetSuperClass")]
+            public virtual object TestMethod() { return new List<object>(); }
+        }
+
+        private interface TargetSuperInterface : SuperTargetSuperInterface
+        {
+            object TestMethod();
+        }
+
+        private interface SuperTargetSuperInterface : SuperInterface
+        {}
+
+        private interface SuperInterface
+        {}
+
+        [Fact]
+        public void TestElementAnnotationScanner()
+        {
+            var scanner = new ElementAnnotationScanner();
+            var annotations = scanner.GetAnnotations(typeof(ClassWithAnnotation));
+            XAssert.NotNull(annotations);
+        }
+
+        [Fact]
+        public void TestFieldAnnotationScanner()
+        {
+            var scanner = new FieldAnnotationScanner();
+            var field = typeof(ClassWithFieldAnnotation).GetField("TestField");
+            XAssert.NotNull(field);
+            var annotations = scanner.GetAnnotations(field);
+            XAssert.NotNull(annotations);
+        }
+
+        [Fact]
+        public void TestGenericAnnotationScanner()
+        {
+            var scanner = new GenericAnnotationScanner(true, true, true);
+            var annotations = scanner.GetAnnotations(typeof(ClassWithAnnotation));
+            XAssert.NotNull(annotations);
+        }
+
+        [Fact]
+        public void TestMetaAnnotationScanner()
+        {
+            var scanner = new MetaAnnotationScanner();
+            var annotations = scanner.GetAnnotations(typeof(RootAnnotation));
+            XAssert.NotNull(annotations);
+        }
+
+        [Fact]
+        public void TestMethodAnnotationScanner()
+        {
+            var scanner = new MethodAnnotationScanner();
+            var method = typeof(TargetClass).GetMethod("TestMethod");
+            XAssert.NotNull(method);
+            var annotations = scanner.GetAnnotations(method);
+            XAssert.NotNull(annotations);
+        }
+
+        [Fact]
+        public void TestTypeAnnotationScanner()
+        {
+            var scanner = new TypeAnnotationScanner();
+            var annotations = scanner.GetAnnotations(typeof(ClassWithAnnotation));
+            XAssert.NotNull(annotations);
+        }
 
         [AnnotationForTest("field-annotation")]
         private class ClassWithFieldAnnotation
