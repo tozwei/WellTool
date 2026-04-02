e615d971a52df7c3cc49139aa6444c3e5d304188
tozwei <tozwei@126.com>
2026-04-02 18:41:30 +0800

    测试通过

--- src/WellTool.Core/Net/Multipart/UploadSetting.cs
+++ src/WellTool.Core/Net/Multipart/UploadSetting.cs
@@ -58,4 +58,57 @@ namespace WellTool.Core.Net.Multipart
 
         /// <summary>
         /// 设定文件保存到内存的边界
-        /// 如果文件大小小于这个边界，将保存
+        /// 如果文件大小小于这个边界，将保存于内存中，否则保存至临时目录中
+        /// </summary>
+        /// <param name="memoryThreshold">文件保存到内存的边界</param>
+        public void SetMemoryThreshold(int memoryThreshold)
+        {
+            _memoryThreshold = memoryThreshold;
+        }
+
+        /// <summary>
+        /// 上传文件的临时目录，若为空，使用系统目录
+        /// </summary>
+        public string TmpUploadPath => _tmpUploadPath;
+
+        /// <summary>
+        /// 设定上传文件的临时目录，null表示使用系统临时目录
+        /// </summary>
+        /// <param name="tmpUploadPath">临时目录，绝对路径</param>
+        public void SetTmpUploadPath(string tmpUploadPath)
+        {
+            _tmpUploadPath = tmpUploadPath;
+        }
+
+        /// <summary>
+        /// 文件扩展名限定列表
+        /// </summary>
+        public string[] FileExts => _fileExts;
+
+        /// <summary>
+        /// 设定文件扩展名限定里列表
+        /// 禁止列表还是允许列表取决于isAllowFileExts
+        /// </summary>
+        /// <param name="fileExts">文件扩展名列表</param>
+        public void SetFileExts(string[] fileExts)
+        {
+            _fileExts = fileExts;
+        }
+
+        /// <summary>
+        /// 是否允许文件扩展名
+        /// 若true表示只允许列表里的扩展名，否则是禁止列表里的扩展名
+        /// </summary>
+        public bool IsAllowFileExts => _isAllowFileExts;
+
+        /// <summary>
+        /// 设定是否允许扩展名
+        /// 若true表示只允许列表里的扩展名，否则是禁止列表里的扩展名
+        /// </summary>
+        /// <param name="isAllowFileExts">是否允许文件扩展名</param>
+        public void SetAllowFileExts(bool isAllowFileExts)
+        {
+            _isAllowFileExts = isAllowFileExts;
+        }
+    }
+}
