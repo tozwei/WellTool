7bc14caa74443de83113c2c437b6580d29027046
tozwei <tozwei@126.com>
2026-04-02 10:41:02 +0800

    增加文件

---
+++ src/WellTool.DB.Tests/HsqldbTest.cs
@@ -0,0 +1,32 @@
+// Copyright (c) 2025 WellTool Team
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Xunit;
+
+namespace WellTool.DB.Tests;
+
+/// <summary>
+/// HSQLDB 数据库测试
+/// </summary>
+public class HsqldbTest
+{
+    /// <summary>
+    /// 测试 HSQLDB 数据库连接
+    /// </summary>
+    [Fact]
+    public void TestHsqldbConnection()
+    {
+        // TODO: 实现测试方法
+        Assert.True(true);
+    }
+}
