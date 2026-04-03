eb3b3775e196cb53e61e5a409a9f164ba08b5183
tozwei <tozwei@126.com>
2026-04-03 21:35:34 +0800

    System.Exception修正

--- src/WellTool.Core/Annotation/BeanException.cs
+++ src/WellTool.Core/Annotation/BeanException.cs
@@ -3,7 +3,7 @@ namespace WellTool.Core.Bean;
 /// <summary>
 /// Bean异常
 /// </summary>
-public class BeanException : Exception
+public class BeanException : System.Exception
 {
 	/// <summary>
 	/// 构造函数
@@ -25,7 +25,7 @@ public class BeanException : Exception
 	/// </summary>
 	/// <param name="message">异常消息</param>
 	/// <param name="innerException">内部异常</param>
-	public BeanException(string message, Exception innerException) : base(message, innerException)
+	public BeanException(string message, System.Exception innerException) : base(message, innerException)
 	{
 	}
 
@@ -44,7 +44,7 @@ public class BeanException : Exception
 	/// <param name="innerException">内部异常</param>
 	/// <param name="format">格式化字符串</param>
 	/// <param name="args">参数</param>
-	public BeanException(Exception innerException, string format, params object[] args) : base(string.Format(format, args), innerException)
+	public BeanException(System.Exception innerException, string format, params object[] args) : base(string.Format(format, args), innerException)
 	{
 	}
 }
